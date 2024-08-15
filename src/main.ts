ce9ede5d81e8403640ce23de68571031c4b2cde3
Ryan Braz Tintore Viana <ryanviana@usp.br>
2024-08-15 19:38:22 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001', // Specify the allowed origin
+    origin: 'https://api.the-prism-app.com', // Specify the allowed origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
